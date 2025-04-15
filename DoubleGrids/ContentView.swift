bcdcd799470272a410a9a024d07d71036625eec5
Francisco Casillas <chiccasillas@gmail.com>
2025-04-15 13:12:58 -0600

    Initial Commit

---
+++ DoubleGrids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoubleGrids
+//
+//  Created by Francisco Jean on 15/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoubleGrids/DoubleGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleGridsApp.swift
+//  DoubleGrids
+//
+//  Created by Francisco Jean on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DoubleGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
